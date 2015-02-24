11d46c6f975d1a5a81bc76eca3a14b2343d7e620
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-24 11:27:43 -0600

    added wiredep kept jade pretty autoprefixer and connect server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,71 @@
-'user strict';
+'use strict';
 
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    autoprefixer: {
+      options: {
+        browsers: ['> 1% in US']
+      },
+
+      build: {
+        src: 'public/css/main.css'
+      }
+    },
     clean: ['public'],
     copy: {
       main: {
         files: [
-          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade', '!**/*.{sass, scss}'],  dest: 'public/', filter: 'isFile'}
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade', '!**/*.{sass,scss}'], dest: 'public/', filter: 'isFile'}
         ]
       }
     },
+
+    connect: {
+      options: {
+        port: 8888,
+        open: true,
+        useAvailablePort: true,
+        hostname: 'localhost'
+      },
+
+      server: {
+        options: {
+          middleware: function (connect) {
+            return [
+              connect.static('public'),
+              connect().use('/scripts', connect.static('./app/scripts')),
+              connect().use('/bower_components', connect.static('./bower_components'))
+            ];
+          }
+        }
+      },
+    },
+
     jade: {
       compile: {
-        files: [{expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'],  dest: 'public/', ext: '.html'}]
+        options: {
+          pretty: true
+        },
+        files: [{expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
       }
     },
     sass: {
       options: {
         sourceMap: true
-        },
-        dist: {
-          files: {
-            'public/css/main.css': 'app/styles/main.scss'
+      },
+      dist: {
+        files: {
+          'public/css/main.css': 'app/styles/main.scss'
         }
       }
     },
     watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
       other: {
         files: ['app/**', '!app/**/*.jade', '!app/**/*.{sass,scss}'],
         tasks: ['copy']
@@ -38,14 +75,19 @@ module.exports = function(grunt) {
         tasks: ['jade']
       },
       sass: {
-        files: ['app/styles/{,*/}*.{scss,sass}'],
-        tasks: ['sass']
-      },
+        files: ['app/**/*.{sass,scss}'],
+        tasks: ['sass', 'autoprefixer']
+      }
     },
 
+    wiredep: {
+      build: {
+        src: ['public/**/*.html']
+      }
+    }
   });
 
   grunt.registerTask('default', []);
-  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass']);
-  grunt.registerTask('serve', ['build', 'watch']);
+  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass', 'autoprefixer', 'wiredep']);
+  grunt.registerTask('serve', ['build', 'connect', 'watch']);
 };
